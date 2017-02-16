ac8d4b2472f7fbc4a87a6264f5e027922a243a5d
chungphing <chungphing@gmail.com>
2017-02-16 23:27:40 +0700

    add new CRUD operation query to Datasource, add new model file( MovieAltID, MovieGenre, MovieHall, MovieShowTime) to store Movie Details.

--- Android/app/src/main/java/ruppy3e1/merl/Datasource/MovieDataSource.java
+++ Android/app/src/main/java/ruppy3e1/merl/Datasource/MovieDataSource.java
@@ -1,8 +1,12 @@
-package ruppy3e1.merl.Database;
+package ruppy3e1.merl.Datasource;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import ruppy3e1.merl.Database.MovieSQLiteHelper;
+import ruppy3e1.merl.Model.Movie;
 
 /**
  * Created by chunpghing
@@ -19,4 +23,40 @@ public class MovieDataSource {
         SQLiteDatabase database = mMovieSQLiteHelper.getReadableDatabase();
         database.close();
     }
+
+    public void create(Movie movie){
+        SQLiteDatabase database = open();
+
+        database.beginTransaction();
+        //implement detail
+        ContentValues movieValues = new ContentValues();
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_NAME, movie.getmName());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_ALT_NAME, movie.getmAltName());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_DESCRIPTION, movie.getmDescription());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_IS_AIRED , movie.getAired());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_IS_SHOWING , movie.getShowing());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_IS_COMMING , movie.getComming());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIES_TRAILER_LINK , movie.getmTrailerLink());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_IMAGE_LINK , movie.getmImgLink());
+        movieValues.put(MovieSQLiteHelper.COLUMN_MOVIE_DETAIL_URL , movie.getmDetailUrl());
+
+        long movieID = database.insert(MovieSQLiteHelper.MOVIES_TABLE, null, movieValues);
+
+        //for (Movie)
+
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        close(database);
+
+    }
+
+    private SQLiteDatabase open(){
+       return mMovieSQLiteHelper.getWritableDatabase();
+    }
+
+
+    private void close(SQLiteDatabase database){
+        database.close();
+    }
+
 }
